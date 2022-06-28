a616f99842920d10117d7f5c4e4884b305537d78
Shafayet Kabir <shafayetk884@gmail.com>
2022-06-29 03:34:48 +0600

    Telegram Messenger

---
+++ Pages/BasePage.py
@@ -0,0 +1,21 @@
+import time
+import unittest
+
+from selenium import webdriver
+
+
+class BasePage(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="E:\chromedriver.exe")
+        cls.driver.maximize_window()
+        cls.driver.get("https://web.telegram.org/k/")
+        print("Test Started")
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(5)
+        # cls.driver.quit()
+        print("Test Complete")
